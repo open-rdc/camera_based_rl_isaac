e2a481a44c093e02b478653d325183cc5a7b5102
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2025-04-28 15:28:46 +0900

    🚧wip: シミュレータ環境構築用スクリプト

---
+++ create_simulator_env.py
@@ -0,0 +1,169 @@
+import argparse
+
+from isaaclab.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(
+    description="Traning for wheeled quadruped robot."
+)
+parser.add_argument("--num_envs", type=int, default=9, help="Number of environments to spawn.")
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+import numpy as np
+import torch
+import os
+
+from isaaclab.envs import ManagerBasedRLEnv
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets import AssetBaseCfg
+from isaaclab.assets.articulation import ArticulationCfg
+from isaaclab.scene import InteractiveScene, InteractiveSceneCfg
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+
+MOBILITY_CONFIG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=os.environ['HOME'] + "/camera_based_rl_isaac/src/robot_description/urdf/mobility/main.usdc",
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            rigid_body_enabled=True,
+            max_linear_velocity=7.0.
+            max_angular_velocity=4.0,
+            max_depenetration_velocity=5.0,
+            enable_gyroscopic=True,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False,
+            # ???
+            solver_position_iteration_count=4,
+            solver_velocity_iteration_count=0,
+            sleep_threshold=0.005,
+            stabilization_threshold=0.001,
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.6, 0.0, 0.0),
+        # orientation
+        rot=(0, 0, -0.682, 0.731),
+        joint_pos={"left_wheel_joint": 0.0, "right_wheel_joint": 0.0, "caster_joint": 0.0},
+    ),
+    actuators={
+        "left_wheel_actuator": ImplicitActuatorCfg(
+            joint_names_expr=["left_wheel_joint"],
+            effort_limit_sim=100.0,
+            velocity_limit_sim=100.0,
+            stiffness=12.0,
+            damping=10.0,
+        ),
+        "right_wheel_actuator": ImplicitActuatorCfg(
+            joint_names_expr=["right_wheel_joint"],
+            effort_limit_sim=100.0,
+            velocity_limit_sim=100.0,
+            stiffness=12.0,
+            damping=10.0,
+        ),
+    },
+)
+
+
+class NewRobotsSceneCfg(InteractiveSceneCfg):
+    """Designs the scene."""
+
+    # Ground-plane
+    ground = AssetBaseCfg(prim_path="/World/defaultGroundPlane", spawn=sim_utils.GroundPlaneCfg())
+
+    # lights
+    dome_light = AssetBaseCfg(
+        prim_path="/World/Light", spawn=sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75))
+    )
+
+    # robot
+    Dofbot = MOBILITY_CONFIG.replace(prim_path="{ENV_REGEX_NS}/Dofbot")
+
+
+def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
+    sim_dt = sim.get_physics_dt()
+    sim_time = 0.0
+    count = 0
+
+    while simulation_app.is_running():
+        # reset
+        if count % 500 == 0:
+            # reset counters
+            count = 0
+            # reset the scene entities to their initial positions offset by the environment origins
+            root_jetbot_state = scene["Jetbot"].data.default_root_state.clone()
+            root_jetbot_state[:, :3] += scene.env_origins
+            root_dofbot_state = scene["Dofbot"].data.default_root_state.clone()
+            root_dofbot_state[:, :3] += scene.env_origins
+
+            # copy the default root state to the sim for the jetbot's orientation and velocity
+            scene["Jetbot"].write_root_pose_to_sim(root_jetbot_state[:, :7])
+            scene["Jetbot"].write_root_velocity_to_sim(root_jetbot_state[:, 7:])
+            scene["Dofbot"].write_root_pose_to_sim(root_dofbot_state[:, :7])
+            scene["Dofbot"].write_root_velocity_to_sim(root_dofbot_state[:, 7:])
+
+            # copy the default joint states to the sim
+            joint_pos, joint_vel = (
+                scene["Jetbot"].data.default_joint_pos.clone(),
+                scene["Jetbot"].data.default_joint_vel.clone(),
+            )
+            scene["Jetbot"].write_joint_state_to_sim(joint_pos, joint_vel)
+            joint_pos, joint_vel = (
+                scene["Dofbot"].data.default_joint_pos.clone(),
+                scene["Dofbot"].data.default_joint_vel.clone(),
+            )
+            scene["Dofbot"].write_joint_state_to_sim(joint_pos, joint_vel)
+            # clear internal buffers
+            scene.reset()
+            print("[INFO]: Resetting Jetbot and Dofbot state...")
+
+        # drive around
+        if count % 100 < 75:
+            # Drive straight by setting equal wheel velocities
+            action = torch.Tensor([[10.0, 10.0]])
+        else:
+            # Turn by applying different velocities
+            action = torch.Tensor([[5.0, -5.0]])
+
+        scene["Jetbot"].set_joint_velocity_target(action)
+
+        # wave
+        wave_action = scene["Dofbot"].data.default_joint_pos
+        wave_action[:, 0:4] = 0.25 * np.sin(2 * np.pi * 0.5 * sim_time)
+        scene["Dofbot"].set_joint_position_target(wave_action)
+
+        scene.write_data_to_sim()
+        sim.step()
+        sim_time += sim_dt
+        count += 1
+        scene.update(sim_dt)
+
+
+def main():
+    """Main function."""
+    # Initialize the simulation context
+    sim_cfg = sim_utils.SimulationCfg(device=args_cli.device)
+    sim = sim_utils.SimulationContext(sim_cfg)
+
+    sim.set_camera_view([3.5, 0.0, 3.2], [0.0, 0.0, 0.5])
+    # design scene
+    scene_cfg = NewRobotsSceneCfg(args_cli.num_envs, env_spacing=2.0)
+    scene = InteractiveScene(scene_cfg)
+    # Play the simulator
+    sim.reset()
+    # Now we are ready!
+    print("[INFO]: Setup complete...")
+    # Run the simulator
+    run_simulator(sim, scene)
+
+
+if __name__ == "__main__":
+    main()
+    simulation_app.close()
