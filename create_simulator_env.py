ab66171e6be560d196fb40d77b6c8adac47ca463
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2025-04-30 09:37:27 +0900

    🐛fix: camera link

--- create_simulator_env.py
+++ create_simulator_env.py
@@ -6,7 +6,7 @@ from isaaclab.app import AppLauncher
 parser = argparse.ArgumentParser(
     description="Traning for wheeled quadruped robot."
 )
-parser.add_argument("--num_envs", type=int, default=9, help="Number of environments to spawn.")
+parser.add_argument("--num_envs", type=int, default=1, help="Number of environments to spawn.")
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
 # parse the arguments
@@ -85,20 +85,8 @@ MOBILITY_CONFIG = ArticulationCfg(
 class CameraBasedRLSceneCfg(InteractiveSceneCfg):
     """Designs the scene."""
 
-    # Ground-plane
-    ground = AssetBaseCfg(
-        prim_path="/World/ground",
-        spawn=sim_utils.GroundPlaneCfg(size=(100.0, 100.0)),
-    )
-
-    # lights
-    dome_light = AssetBaseCfg(
-        prim_path="/World/Light",
-        spawn=sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75)),
-    )
-
-    # AI-Mobility-Park world config
-    aimobility_park_world = TerrainImporterCfg(
+    # AI-Mobility-Park config
+    mobility_park = TerrainImporterCfg(
         prim_path="/World/Terrain",
         terrain_type="usd",
         usd_path=os.environ['HOME'] + "/camera_based_rl_isaac/assets/worlds/usd/mobility_park.usd",
@@ -114,10 +102,22 @@ class CameraBasedRLSceneCfg(InteractiveSceneCfg):
 
     # robot 
     ## prim_path ???
-    mobility: ArticulationCfg = MOBILITY_CONFIG.replace(prim_path="/World/envs/env_0/mobility")
+    mobility: ArticulationCfg = MOBILITY_CONFIG.replace(prim_path="{ENV_REGEX_NS}/Mobility")
 
-    zed_camera = TiledCameraCfg(
-        prim_path="/World/envs/env_0/base_link/camera",
+    # Ground-plane
+    ground = AssetBaseCfg(
+        prim_path="/World/ground",
+        spawn=sim_utils.GroundPlaneCfg(size=(100.0, 100.0)),
+    )
+
+    # lights
+    dome_light = AssetBaseCfg(
+        prim_path="/World/Light",
+        spawn=sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75)),
+    )
+    
+    tiled_camera = TiledCameraCfg(
+        prim_path="{ENV_REGEX_NS}/Mobility/base_link/Camera",
         offset=TiledCameraCfg.OffsetCfg(
             pos=(0.35, 0.0, 0.55),
             rot=(0, 0, 0, 0),
@@ -147,7 +147,7 @@ class ObservationsCfg:
         """Observations for policy group."""
 
         # observation terms (order preserved)
-        image = ObsTerm(func=mdp.image, params={"sensor_cfg": SceneEntityCfg("zed_camera"), "data_type": "rgb"})
+        image = ObsTerm(func=mdp.image, params={"sensor_cfg": SceneEntityCfg("tiled_camera"), "data_type": "rgb"})
 
         def __post_init__(self) -> None:
             self.enable_corruption = False
@@ -173,7 +173,7 @@ class RewardsCfg:
     running_reward = RewTerm(
         func=mdp.compute_reward,
         weight=1.0,
-        params={"asset_cfg": SceneEntityCfg("zed_camera")},
+        params={"asset_cfg": SceneEntityCfg("tiled_camera")},
         )
 
 
@@ -184,7 +184,7 @@ class TerminationsCfg:
 
     # robot_out_of_course = DoneTerm(
     #     func=mdp.termination,
-    #     params={"asset_cfg": SceneEntityCfg("zed_camera"), "minimum_ratio": 0.05},
+    #     params={"asset_cfg": SceneEntityCfg("tiled_camera"), "minimum_ratio": 0.05},
     # )
 
 
@@ -241,7 +241,6 @@ def main():
     env = ManagerBasedRLEnv(cfg=env_cfg)
     # simulate physics
     
-
     count = 0
     while simulation_app.is_running():
         with torch.inference_mode():
